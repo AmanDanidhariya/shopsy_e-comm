6463527b9b9a022116e749dfe5d7f1a8aa57b53a
Aman Danidhariya <amandanidhariya007@gmail.com>
2023-10-24 23:18:00 +0530

    add card for cart and   add to cart functionality

--- src/components/singleProduct/SingleProduct.jsx
+++ src/components/singleProduct/SingleProduct.jsx
@@ -1,33 +1,34 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
-// import {storeData} from '../../assets/data/dummyData'
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../../slices/cartSlice";
 import { useParams } from "react-router-dom";
 import Footer from "../footer/Footer";
 
 const SingleProduct = () => {
+  //select product value from store
   const product = useSelector((state) => state.products.products);
   const { id } = useParams();
+  const dispatch = useDispatch();
 
+  //default size first in sizeArray
   const productSize = product[0].size ? product[0].size[0] : "";
+  //default color first in colorArray
   const productColor = product[0].color[0];
 
+  //get size value
   const [size, setSize] = useState(productSize);
+  //get color value
   const [color, setColor] = useState(productColor);
-  console.log(size);
 
   //find the selected product from data
   const singleProduct = product.find((item) => item.id === id);
   console.log("singleProduct", singleProduct);
 
-
   return (
     <>
       <div className="card lg:card-side bg-base-100 shadow-xl w-[80%] p-4 m-auto mb-6">
         <figure className="w-full h-screen ">
-          <img
-            src={singleProduct.img}
-            alt={singleProduct.name}
-          />
+          <img src={singleProduct.img} alt={singleProduct.name} />
         </figure>
         <div className=" flex flex-col mt-20  ml-10">
           <h2 className="card-title text-black text-4xl py-3">
@@ -60,7 +61,7 @@ const SingleProduct = () => {
             <select
               id={singleProduct.id}
               name="size"
-              value={size}
+              value={color}
               onChange={(e) => setColor(e.target.value)}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
@@ -77,10 +78,29 @@ const SingleProduct = () => {
             $ {singleProduct.price}
           </div>
           {/* list of color end */}
-          <button className="btn btn-primary my-10 hover:text-black">Add to cart</button>
+          <button
+            className="btn btn-primary my-10 hover:text-black"
+            onClick={() =>
+              dispatch(
+                addToCart({
+                  id: singleProduct.id,
+                  price: singleProduct.price,
+                  amount: 1,
+                  totalPrice: singleProduct.price,
+                  name: singleProduct.name,
+                  size: size,
+                  color: color,
+                  img: singleProduct.img,
+                  text: singleProduct.text,
+                })
+              )
+            }
+          >
+            Add to cart
+          </button>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 };
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { useSelector } from "react-redux";
 
 const Cart = () => {
+  const cart = useSelector((state) => state.cart.cart);
+  const totalPrice = useSelector((state) => state.cart.totalPrice);
+  console.log(cart);
+  //id, price,size,amount,img,totalPrice,name,text,color
   return (
-    <div>Cart</div>
-  )
-}
+    <>
+      <div className="flex flex-wrap p-4">
+        {cart.map((item) => {
+          const color = item.color;
+          return (
+            <div
+              className="card card-compact w-96 m-4 shadow-xl bg-gray-300 max-h-[600px]"
+              key={item.id}
+            >
+              <figure>
+                <img src={item.img} alt={item.name} />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{item.name}</h2>
+                <p className="font-semibold">{item.text}</p>
+                <span
+                  className="text-xl font-bold text-white w-2 h-2 p-3 rounded-lg"
+                  style={{ backgroundColor: color }}
+                ></span>
+                <div className="flex justify-between w-full">
+                  <span className="text-2xl font-bold text-purple-800">
+                    {item.amount} pcs.
+                  </span>
+                  <span className="text-2xl font-bold mr-6">{item.size}</span>
+                </div>
+                <div className="flex leading-10">
+                  <p className="text-2xl font-bold text-blue-900 mt-2">
+                    ${item.price}
+                  </p>
+                  <button className="btn btn-error hover:text-white">
+                    remove
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="text-3xl font-bold text-center border-t-[10px] ">
+        <span className="text-black">TotalPrice :- </span>
+        <span className="text-blue-950">$ {totalPrice}</span>
+      </div>
+    </>
+  );
+};
 
-export default Cart
+export default Cart;
